5a21a668f2ef9985526231045dd4bf61f5814941
Ali Churcher <ali@liefery.com>
2018-02-12 15:36:01 +0100

    added styling and new pages

--- sample_app/app/helpers/application_helper.rb
+++ sample_app/app/helpers/application_helper.rb
@@ -3,6 +3,8 @@ module ApplicationHelper
     base_title = "Ruby on Rails Tutorial Sample App"
     if page_title.empty?
       base_title
-    else 
-      page_title + '|' + base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
